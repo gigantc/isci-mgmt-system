118e5a8bf3e0b24e5a894dd9e7e76244add37318
dfree <dan@gigantc.com>
2025-11-12 15:44:35 -0700

    fixing the favicon

--- app/root.jsx
+++ app/root.jsx
@@ -28,6 +28,7 @@ export function Layout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/svg+xml" href="./favicon.svg" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
